75da5ed999f54be1476eda01766e28f1f5c42758
perry <164530379@qq.com>
2019-06-12 21:10:14 +0800

    第二次修改

--- git-test/src/First.java
+++ git-test/src/First.java
@@ -3,6 +3,7 @@ public class First {
         System.out.println("Git-Test1");
         System.out.println("Git-Test2");
         System.out.println("Git-Test3");
+        new First().test();
     }
     public void test(){
         System.out.println("杨超越");
